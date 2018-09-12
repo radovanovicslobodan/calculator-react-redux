ac1e6f36f4ef7a0628982bac23c5a28fe8272fbc
Boban <r.slobodan75@gmail.com>
2018-09-12 17:30:32 +0200

    add redux and refactor components

--- src/components/Calculator/index.js
+++ src/components/Calculator/index.js
@@ -1,30 +1,27 @@
-import React, { Component } from 'react';
-import './Calculator.css';
-import Display from '../Display';
-import ControlPanel from '../ControlPanel';
-import Digits from '../Digits';
-import Operators from '../Operators';
-import History from '../History';
-import ee from '../../eventEmitter';
-import store from '../../store';
+import React, { Component } from "react";
+import "./Calculator.css";
+import Display from "../Display";
+import ControlPanel from "../ControlPanel";
+import Digits from "../Digits";
+import Operators from "../Operators";
+import History from "../History";
+import store from "../../store";
 
 class Calculator extends Component {
-    constructor(props) {
-        super(props);        
-        ee.addListener('displayUpdate', () => this.forceUpdate());
-    }
-    
-    render() {
-        return (
-            <main className="react-calculator">
-                <Display text={store.curExpression} />
-                <ControlPanel />
-                <Digits />
-                <Operators />
-                <History />
-            </main>
-        )
-    }
+  render() {
+    return (
+      <main className="react-calculator">
+        <Display text={store.getState().curExpression} />
+        <ControlPanel />
+        <Digits />
+        <Operators />
+        <History
+          showHistory={store.getState().showHistory}
+          history={store.getState().history}
+        />
+      </main>
+    );
+  }
 }
 
 export default Calculator;
--- src/components/ControlPanel/index.js
+++ src/components/ControlPanel/index.js
@@ -1,29 +1,26 @@
 import React from 'react';
-import ee from '../../eventEmitter';
 import store from '../../store';
 import Button from '../Button';
+import { updateExpression } from '../../actions/expression';
+import { toggleHistory } from '../../actions/history';
 
-export const showHistory = () => {
-    ee.emitEvent('toggle-history');
-}
+export const showHistory = () => toggleHistory();
 
-export const clearDisplay = () => {
-    store.newExpression = 0;
-}
+export const clearDisplay = () => updateExpression(0);
 
 export const removeOneChar = () => {
-    const curExpression = String(store.curExpression);
+    const curExpression = String(store.getState().curExpression);
     const newExpWithRemovedChar = curExpression.toString().trim().substring(0, (curExpression.length - 1));
 
-    return store.newExpression = newExpWithRemovedChar === '' ? 0 : newExpWithRemovedChar;
+    return updateExpression(newExpWithRemovedChar === '' ? 0 : newExpWithRemovedChar);
 }
 
 
 export const ControlPanel = () => (
     <section className="buttons--controls">
-        <Button buttonClass="control" text="&larr;" clickHandler={this.removeOneChar} />
-        <Button buttonClass="control" text="c" clickHandler={this.clearDisplay} />
-        <Button buttonClass="control" text="history" clickHandler={this.showHistory} />
+        <Button buttonClass="control" text="&larr;" clickHandler={removeOneChar} />
+        <Button buttonClass="control" text="c" clickHandler={clearDisplay} />
+        <Button buttonClass="control" text="history" clickHandler={showHistory} />
     </section>
 );
 
